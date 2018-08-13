31c357283b85a5b08d778c62f6aa22fc145ad9f5
someone <someone@someplace.com>
2018-08-13 12:54:48 +0000

    stock update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -85,6 +85,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:title, :description, :price, :image_url, :category, :brand)
+      params.require(:item).permit(:title, :description, :price, :image_url, :category, :brand,:stock)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180811233216) do
+ActiveRecord::Schema.define(version: 20180813114144) do
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20180811233216) do
     t.string   "brand"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "stock"
+    t.string   "integer"
   end
 
   create_table "profiles", force: :cascade do |t|

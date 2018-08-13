a1b59cb33ea3db81daed868846fe5503f9975e61
someone <someone@someplace.com>
2018-08-13 15:12:41 +0000

    db backup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180813114144) do
+ActiveRecord::Schema.define(version: 20180813145343) do
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
